761813824423780cf9aad4529638a807f0e049d7
Emilio <emilio.deltessa@gmail.com>
2013-03-25 15:23:44 +0100

    added conversion scripts

--- test/aa1/convert.py
+++ test/aa1/convert.py
@@ -6,18 +6,18 @@ import fileinput
 import sys
 
 def convert ( ):
-	for line in fileinput.input():
-		if line.startswith( "#" ) or not line.strip():
-			continue
-		
-		rowid, *inputs, targetx, targety = line.rstrip().split(',')
-		
-		
-		sys.stdout.write( targetx + " " + targety + " " )
-		for num in inputs:
-			sys.stdout.write( "" + num + " " )
-		
-		sys.stdout.write( "\n" )
+  for line in fileinput.input():
+    if line.startswith( "#" ) or not line.strip():
+      continue
+
+    rowid, *inputs, targetx, targety = line.rstrip().split(',')
+
+    #sys.stdout.write( targetx + " " + targety + " " )
+    sys.stdout.write( "{:10.7} {:10.7} ".format( float(targetx), float(targety) ) )
+    for num in inputs:
+      sys.stdout.write( "{:10.7}".format( float(num) ) )
+
+    sys.stdout.write( "\n" )
 
 if __name__ == '__main__':
-	convert( )
+  convert( )
